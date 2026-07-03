347a01530383828ffe154fb256ee6e2f2110d7ae
ted-cc6 <60936933+ted-cc6@users.noreply.github.com>
2026-07-03 14:43:00 -0500

    House Keeping

--- Test1.py
+++
@@ -1,6 +0,0 @@
-from data_loader.data_loader_api import load_survey_data
-ds = load_survey_data("2026_Q2")
-
-print(f"ds.insured_event_base rows: {len(ds.insured_event_base)}")
-insured_yes = (ds.insured_event_base["q_child_wellbeing"] == "Yes").sum()
-print(f"Yes in insured_event_base: {insured_yes}")
